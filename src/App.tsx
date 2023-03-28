792fc536581a7842563f68918a7213e52d33a513
Erfan Amiri <erfanabedian@geotab.com>
2023-03-28 15:07:42 +0000

    added expandable text

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,14 @@ import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  let items = ["Tehran", "New York", "Waterloo", "Montreal"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-  const [alertVisible, setAlertVisibility] = useState(false);
+  const text =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
   return (
     <>
-      <div>
-        <ListGroup
-          items={items}
-          heading="Cities"
-          onSelectItem={(item) => handleSelectItem(item)}
-        />
-      </div>
-      <div>
-        {alertVisible && (
-          <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
-        )}
-      </div>
-      <div>
-        <Button
-          onClick={() => {
-            setAlertVisibility(alertVisible ? false : true);
-          }}
-          color="primary"
-        >
-          My Button
-        </Button>
-      </div>
+      <ExpandableText>{text}</ExpandableText>
     </>
   );
 }

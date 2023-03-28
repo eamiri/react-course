3e8dbfec339db1e373a565bcc696153751f8979c
eamiri <er.abedian@gmail.com>
2023-03-28 01:18:45 +0000

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import ListGroup from './components/ListGroup';
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  return <div><ListGroup /></div>;
+  let items = ["Tehran", "New York", "Waterloo", "Montreal"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <>
+      <div>
+        <ListGroup
+          items={items}
+          heading="Cities"
+          onSelectItem={(item) => handleSelectItem(item)}
+        />
+      </div>
+      <div>
+        {alertVisible && (
+          <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
+        )}
+      </div>
+      <div>
+        <Button
+          onClick={() => {
+            setAlertVisibility(alertVisible ? false : true);
+          }}
+          color="primary"
+        >
+          My Button
+        </Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
